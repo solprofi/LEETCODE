51d1d0570081e57b48c2e22f5a83f1dc81c44441
Roman Solohor <rsolohor@tango.me>
2024-10-25 14:10:12 +0200

    125. Valid Palindrome.

---
+++ Two Pointers/Valid Palindrome.js
@@ -0,0 +1,32 @@
+const isAlphanumeric = str => {
+    const charValue = str.charCodeAt(0);
+
+    return (charValue >= 'a'.charCodeAt(0) && charValue <= 'z'.charCodeAt(0)) ||
+        (charValue >= '0'.charCodeAt(0) && charValue <= '9'.charCodeAt(0));
+}
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = str => {
+    const filteredStr = str
+        .toLowerCase()
+        .split('')
+        .filter(isAlphanumeric)
+        .join('');
+
+    let left = 0;
+    let right = filteredStr.length - 1;
+
+    while (left <= right) {
+        if (filteredStr[left] !== filteredStr[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
