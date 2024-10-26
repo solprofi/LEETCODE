b52d5dc60812c48ce8bc4eca5f33f9984f3645c5
Roman Solohor <rsolohor@tango.me>
2024-10-26 13:59:50 +0200

    11. Container With Most Water.

---
+++ Two Pointers/Container With Most Water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = height => {
+    let max = 0;
+    let left = 0;
+    let right  = height.length - 1;
+
+    while (left < right) {
+        const area = (right - left) * Math.min(height[left], height[right]);
+        max = Math.max(max, area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return max;
+};
