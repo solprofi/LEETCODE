93f22b96aefc097f58c79955ae419e25fd82d880
Roman Solohor <rsolohor@tango.me>
2024-10-31 16:22:30 +0100

    567. Permutation in String. Add O(n) solution

--- Sliding Window/Permutation in String.js
+++ Sliding Window/Permutation in String.js
@@ -42,3 +42,67 @@ const checkInclusion = (s1, s2) => {
 
     return areMapsEqual(s1Map, substrMap);
 }
+
+
+const initMap = () => {
+    const map = {};
+
+    for (let i = 0; i < 26; i++) {
+        const key = String.fromCharCode(97 + i);
+        map[key] = 0;
+    }
+
+    return map;
+}
+
+// O(n) solution
+const checkInclusion2 = (s1, s2) => {
+    const s1Map = initMap();
+    const substrMap = initMap();
+
+    let left = 0;
+    let right = 0;
+    let matchesCount = 0;
+
+    while (right < s1.length) {
+        s1Map[s1[right]]++
+        substrMap[s2[right]]++;
+
+        right++;
+    }
+
+    for (let i = 0; i < 26; i++) {
+        const char = String.fromCharCode(i + 97);
+
+        if (s1Map[char] === substrMap[char]) {
+            matchesCount++;
+        }
+    }
+
+    while (right < s2.length) {
+        if (matchesCount === 26) {
+            return true;
+        }
+
+        const rightChar = s2[right];
+        substrMap[rightChar]++;
+        if (substrMap[rightChar] > s1Map[rightChar]) {
+            matchesCount--;
+        } else if (substrMap[rightChar] === s1Map[rightChar]) {
+            matchesCount++;
+        }
+
+        const leftChar = s2[left];
+        substrMap[leftChar]--;
+        if (substrMap[leftChar] < s1Map[leftChar]) {
+            matchesCount--;
+        } else if (substrMap[leftChar] === s1Map[leftChar]) {
+            matchesCount++;
+        }
+
+        left++;
+        right++;
+    }
+
+    return matchesCount === 26;
+}
