b0863ca124b6b1c054e5ee0f87d9903c4dc483b9
Roman Solohor <rsolohor@tango.me>
2024-11-21 13:46:42 +0100

    142. Linked List Cycle II with O(1) space.

--- Linked List/Linked List Cycle II.js
+++ Linked List/Linked List Cycle II.js
@@ -18,3 +18,32 @@ const detectCycle = head => {
 
     return null;
 }
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// O(1) space with T&H algorithm
+const detectCycle = head => {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            slow = head;
+
+            while (slow !== fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            return slow;
+        }
+    }
+
+    return null;
+}
