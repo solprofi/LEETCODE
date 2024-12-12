03498db2be0a21fd2817c8dfcd3b6adf94d326ee
Roman Solohor <rsolohor@tango.me>
2024-12-12 21:24:05 +0100

    Add postorder traversal with one stack.

---
+++ Trees/Postorder traversal.js
@@ -0,0 +1,84 @@
+// recursive
+const postorderTraversal = root => {
+  const result = [];
+
+  const dfs = node => {
+    if (!node) {
+      return;
+    }
+
+    dfs(node.left);
+    dfs(node.right);
+
+    result.push(node.val);
+  }
+
+  dfs(root);
+
+  return result;
+}
+
+//two stacks
+const postOrder2Stacks = root => {
+  if (!root) {
+    return [];
+  }
+
+  const result = [];
+  const stack1 = [root];
+  const stack2 = [];
+
+  while (stack1.length) {
+    const curr = stack1.pop();
+    stack2.push(curr);
+
+    if (curr.left) {
+      stack1.push(curr.left);
+    }
+
+    if (curr.right) {
+      stack1.push(curr.right);
+    }
+  }
+
+  while (stack2.length) {
+    const curr = stack2.pop();
+    result.push(curr.val);
+  }
+
+  return result;
+}
+
+//iterative with one stack
+const postOrder1Stack = root => {
+  if (!root) {
+    return [];
+  }
+
+  const result = [];
+  const stack = [];
+  let curr = root;
+
+  while (curr || stack.length) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+
+    let temp = stack[stack.length - 1].right;
+
+    if (temp) {
+      curr = temp;
+    } else {
+      temp = stack.pop();
+      result.push(temp.val);
+
+      while (stack.length && temp === stack[stack.length - 1].right) {
+        temp = stack.pop();
+        result.push(temp.val);
+      }
+    }
+  }
+
+  return result;
+}
