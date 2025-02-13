e5cdf878a09110f00d3d3b7ee9ab6b4f2e83bda5
solprofi <solohor.roman@gmail.com>
2025-02-13 18:44:26 +0100

    563. Binary Tree Tilt

---
+++ Trees/Binary Tree Tilt.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const findTilt = root => {
+  let res = 0;
+
+  const dfs = node => {
+    if (!node) {
+      return 0;
+    }
+
+    const leftSum = dfs(node.left);
+    const rightSum = dfs(node.right);
+
+    res += Math.abs(leftSum - rightSum);
+
+    return leftSum + rightSum + node.val;
+  }
+
+  dfs(root);
+
+  return res;
+}
